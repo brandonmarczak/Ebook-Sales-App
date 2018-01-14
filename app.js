6851f4f91f00f73fa26fd9885d6da6f80c3600e5
Brandon Marczak <brandonmarczak@yahoo.co.uk>
2018-01-14 14:13:15 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
 const express = require('express');
 const keys = require('./config/keys');
-const stripe = require('stripe')('keys.stripeSecretKey');
+const stripe = require('stripe')(keys.stripeSecretKey);
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
 const app = express();
 
-//Handle bars Middleware
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
+// Handlebars Middleware
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-//Set Static Folder
-app.use(express.static('${__dirname}/public'));
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
 
 // Index Route
 app.get('/', (req, res) => {
-	res.render('index', {
-
-		stripePublishableKey: keys.stripePublishableKey
-	});
+  res.render('index', {
+    stripePublishableKey: keys.stripePublishableKey
+  });
 });
 
-app.get('/success', (req, res) =>{
-	res.render('success');
-});
-
-//Charge Route
-app.post('/charge', (req,res) => {
-	const amount = 2500;
-
-	stripe.customers.create({
-		email: req.body.stripeEmail,
-		source: req.body.stripeToken
-	})
-	.then(customer => stripe.charges.create({
-		amount,
-		description:'web development ebook',
-		currency:'usd',
-		customer:customer.id
-	}))
-	.then(charge => res.render('success'));
+// Charge Route
+app.post('/charge', (req, res) => {
+  const amount = 2500;
+  
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'Web Development Ebook',
+    currency: 'usd',
+    customer: customer.id
+  }))
+  .then(charge => res.render('success'));
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log('Server started on port ${port}');
+  console.log(`Server started on port ${port}`);
 });
 
 
--- config/key_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	stripePublishableKey: 'pk_test_zxkSrCxHqoXZqDi9BZxnyLWt',
-	stripeSecretKey: 'sk_test_nMHFhxTWEm9wfmZSD6B20cLa';
-}
